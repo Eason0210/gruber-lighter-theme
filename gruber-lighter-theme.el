90e9b601b477c3b16f0d71b5e8dd8002db974138
Eason0210 <aqua0210@163.com>
2026-01-20 20:57:33 +0800

    Remove comment

--- gruber-lighter-theme.el
+++ gruber-lighter-theme.el
@@ -49,7 +49,7 @@
       (gruber-lighter-bg+3       "#b8bcc0")
       (gruber-lighter-bg+4       "#a8a8a8")
       (gruber-lighter-wheat      "#fee0b8")
-      (gruber-lighter-Red-violet "#c73a82") ;; Python decorators or triple string,Here-doc string
+      (gruber-lighter-Red-violet "#c73a82")
       (gruber-lighter-red-1      "#f43841")
       (gruber-lighter-red        "#d02324")
       (gruber-lighter-red+1      "#aa0000")
